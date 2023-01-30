048c0d7ab6858b5f209c93148a343a98c219cc4d
Gary Feng <104392015+gfeng-certik@users.noreply.github.com>
2023-01-30 16:22:03 -0500

    fix: keep information from AST about a TupleExpression being an inline array and generate SlithIR accordingly (#26)
    CertiKProject/slither-task#212

--- slither/core/expressions/tuple_expression.py
+++ slither/core/expressions/tuple_expression.py
@@ -4,15 +4,21 @@ from slither.core.expressions.expression import Expression
 
 
 class TupleExpression(Expression):
-    def __init__(self, expressions: List[Expression]) -> None:
+    def __init__(self, expressions: List[Expression], is_inline_array: bool = False) -> None:
         assert all(isinstance(x, Expression) for x in expressions if x)
         super().__init__()
         self._expressions = expressions
+        self._is_inline_array = is_inline_array
 
     @property
     def expressions(self) -> List[Expression]:
         return self._expressions
 
+    @property
+    def is_inline_array(self) -> bool:
+        return self._is_inline_array
+
     def __str__(self) -> str:
         expressions_str = [str(e) for e in self.expressions]
-        return "(" + ",".join(expressions_str) + ")"
+        l_bracket, r_bracket = ("[","]") if self._is_inline_array else ("(",")")
+        return l_bracket + ",".join(expressions_str) + r_bracket
