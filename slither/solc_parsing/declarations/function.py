d2618770beaf74471125177d32a51de141686d25
Kevin Clancy <kevin.clancy@certik.com>
2023-02-09 17:50:26 -0800

    name unnamed variables after the parameter index number

--- slither/solc_parsing/declarations/function.py
+++ slither/solc_parsing/declarations/function.py
@@ -1,4 +1,5 @@
 import logging
+import pdb
 from typing import Dict, Literal, Optional, Union, List, TYPE_CHECKING
 
 from slither.core.expressions.tuple_expression import TupleExpression
@@ -1332,13 +1333,13 @@ class FunctionSolc(CallerContextExpression):
         else:
             params = params[self.get_children("children")]
 
-        next_unnamed_param_id = 0
+        param_number = 0
         for param in params:
             assert param[self.get_key()] == "VariableDeclaration"
             local_var = self._add_param(param)
             if self.slither_parser.generates_certik_ir and local_var.underlying_variable.name == "":
-                local_var.underlying_variable.name = f"ANON_IN_{next_unnamed_param_id}"
-                next_unnamed_param_id += 1
+                local_var.underlying_variable.name = f"IN_{param_number}"
+            param_number += 1
             self._function.add_parameters(local_var.underlying_variable)
 
     def _parse_returns(self, returns: Dict):
@@ -1352,13 +1353,13 @@ class FunctionSolc(CallerContextExpression):
         else:
             returns = returns[self.get_children("children")]
 
-        next_unnamed_ret_id = 0
+        ret_param_number = 0
         for ret in returns:
             assert ret[self.get_key()] == "VariableDeclaration"
             local_var = self._add_param(ret)
             if self.slither_parser.generates_certik_ir and local_var.underlying_variable.name == "":
-                local_var.underlying_variable.name = f"ANON_OUT_{next_unnamed_ret_id}"
-                next_unnamed_ret_id += 1
+                local_var.underlying_variable.name = f"OUT_{ret_param_number}"
+            ret_param_number += 1
             self._function.add_return(local_var.underlying_variable)
 
     def _parse_modifier(self, modifier: Dict):
