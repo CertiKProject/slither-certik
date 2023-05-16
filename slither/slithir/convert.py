4d707de01010a77e875550e2eb259efca2e61516
Kevin Clancy <kevin.clancy@certik.com>
2023-05-16 14:49:40 -0700

    fixed structure of convert_to_structure_to_list function

--- slither/slithir/convert.py
+++ slither/slithir/convert.py
@@ -1620,9 +1620,12 @@ def _convert_to_structure_to_list(return_type: Type) -> List[Type]:
     #         (uint a, uint b) = a.st(0);
     #     }
     # }
-    if isinstance(return_type, (MappingType, ArrayType)):
+    elif isinstance(return_type, (MappingType, ArrayType)):
         return []
-    return [return_type]
+    elif isinstance(return_type, ElementaryType):
+        return [return_type]
+    else:
+        assert False
 
 
 def convert_type_of_high_and_internal_level_call(
