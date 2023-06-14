648ec3ed9b15c5dbcf75c69db8493703190c4b14
Kevin Clancy <kevin.clancy@certik.com>
2023-06-14 10:25:42 -0700

    fix: only set type if field is in struct type (#51)
    https://github.com/CertiKProject/slither-task/issues/566

--- slither/visitors/slithir/expression_to_slithir.py
+++ slither/visitors/slithir/expression_to_slithir.py
@@ -535,7 +535,12 @@ class ExpressionToSlithIR(ExpressionVisitor):
                 return
 
         val = ReferenceVariable(self._node)
-        if isinstance(expr, Variable) and isinstance(expr.type, UserDefinedType) and isinstance(expr.type.type, Structure):
+        if (
+            isinstance(expr, Variable)
+            and isinstance(expr.type, UserDefinedType)
+            and isinstance(expr.type.type, Structure)
+            and expression.member_name in expr.type.type.elems
+        ):
             val.set_type(expr.type.type.elems[expression.member_name].type)
         member = Member(expr, Constant(expression.member_name), val)
         member.set_expression(expression)
